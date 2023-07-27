d3c9a05ebc571e168e982067a3021f477f10c037
Deepak <dgusaiwal537@gmail.com>
2023-07-27 17:36:46 +0530

    add totorial file link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import EditTodo from "./components/EditTodo";
+// Totorial Page Link :- https://dev-academy.com/react-firebase/
 const App = () => {
   return (
     <>
